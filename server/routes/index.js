345a08846285857d283925c62d8c28030f2d2794
Temitope <tope_olajide@outlook.com>
2019-03-09 16:25:06 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import businesses from './diary';
 import users from './user';
 
 const routes = express.Router();
 
 routes.use('/user', users);
-routes.use('/business', businesses);
+
 
 export default routes;
