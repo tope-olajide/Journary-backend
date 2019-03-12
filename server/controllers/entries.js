2d59c77cf0114e3db04dfc3051d7f5787a617de7
Temitope <tope_olajide@outlook.com>
2019-03-12 09:45:09 +0100

    Add deleteEntry Controller

--- server/controllers/entries.js
+++ server/controllers/entries.js
@@ -104,4 +104,38 @@ export default class Entry {
       });
     }
   }
+
+  static async deleteEntry({
+    params,
+    user
+  }, res) {
+    const userId = user.userid;
+    const { entryId } = params;
+    const findEntryQuery = 'SELECT * FROM entries WHERE entryId=$1';
+    const deleteQuery = 'DELETE FROM entries WHERE entryId=$1 returning *';
+    try {
+      const { rows } = await db.query(findEntryQuery, [entryId]);
+      if (!rows[0]) {
+        return res.status(404).json({
+          success: false,
+          message: 'diary does not exist!'
+        });
+      }
+      if (rows[0].userid !== userId) {
+        return res.status(401).json({
+          success: false,
+          message: 'You cannot modify diary not created by You!'
+        });
+      }
+      const deleteDairy = await db.query(deleteQuery, [entryId]);
+      if (deleteDairy.rows[0]) {
+        res.status(200).json({
+          success: true,
+          message: 'Diary Deleted!'
+        });
+      }
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
 }
