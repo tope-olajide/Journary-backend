8c0663c7d2dd0b94f558b065faf2af5ae62bce21
Temitope <tope_olajide@outlook.com>
2019-03-20 11:07:22 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AuthPage from './components/Auth/AuthPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+      <head>
+  <meta charset="UTF-8"/>
+  <title>My Dairy</title>
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+</head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+  crossorigin="anonymous"/>
+<link href="https://fonts.googleapis.com/css?family=Alegreya" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Eczar" rel="stylesheet"/>
+<AuthPage />
+</>
     );
   }
 }
