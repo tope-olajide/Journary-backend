2919a9fc84d8fd7313d9ccb0e74fefa7c464e3e8
Temitope <tope_olajide@outlook.com>
2019-03-21 01:44:33 +0100

    Create Footer.jsx

---
+++ client/src/components/commons/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <>
+      <footer class="footer">
+        <p class=" ">
+          Copyright © 2019 My-Diary. All rights reserved | Designed with{" "}
+          <i class="fas fa-heart fa-lg" /> by
+          <a href="https://github.com/tope-olajide">Temitope</a>{" "}
+        </p>
+      </footer>
+    </>
+  );
+};
+export default Footer;
