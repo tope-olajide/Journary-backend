f1059bab4bf29e83a9aa1595ddb75fa379ff8624
Temitope <tope_olajide@outlook.com>
2019-03-23 15:53:17 +0100

    Update index.jsx

--- client/src/components/Auth/index.jsx
+++ client/src/components/Auth/index.jsx
@@ -6,8 +6,10 @@ import Intro from "./Intro";
 import Footer from "../commons/Footer";
 import { validateUser } from "../../utils/validator";
 import toastNotification from "./../../utils/toastNotification";
+import { signUp,signIn } from "../../actions/authActions";
+import { connect } from 'react-redux';
 
-function Authpage() {
+function Authpage(props) {
   const [showSignup, setShowSignup] = useState(true);
   const [showSignin, setShowSignin] = useState(false);
   const [userSignUpDetails, setUserSignUpDetails] = useState({
@@ -17,10 +19,27 @@ function Authpage() {
     password: "",
     repeatPassword: ""
   });
-
+  const [userSignInDetails, setUserSignInDetails] = useState({
+    authname: "",
+    password: ""
+  });
+const [isLoading, setIsLoading] = useState(false);
+const [submitValue, setSubmitValue] = useState("Submit");
+const enableLoading=()=>{
+  setSubmitValue(<b><i class="fas fa-spinner fa-lg fa-spin"></i></b>)
+  setIsLoading(true)
+}
+const disableLoading=()=>{
+  setSubmitValue("Submit")
+  setIsLoading(false)
+}
   const saveSignupToState = (key, value) => {
     setUserSignUpDetails({ ...userSignUpDetails, [key]: value });
     console.log(userSignUpDetails);
+  };
+  const saveSigninToState = (key, value) => {
+    setUserSignInDetails({ ...userSignInDetails, [key]: value });
+    console.log(userSignInDetails);
   };
   const toggleSignup = () => {
     if (showSignin) {
@@ -41,7 +60,54 @@ function Authpage() {
     if (validateSignupError) {
       toastNotification(["error"],validateSignupError);
     }
+    else if (userSignUpDetails.password !== userSignUpDetails.repeatPassword) {
+      toastNotification(["error"],"passwords does not match");
+    } else {
+      toastNotification(["info"],'Signing you up...')
+      enableLoading()
+      props.signUp(userSignUpDetails).then(
+        () => {
+          toastNotification(["success"],`Welcome <br/><em>${userSignUpDetails.username}</em>`);
+/*           setTimeout(() => {
+            window.location = "/";
+          }, 300); */
+          disableLoading()
+        },
+        error => {
+          if(!error.response){
+            toastNotification(["error"], 'Network Error');
+            disableLoading()
+          }
+          else{
+            toastNotification(["error"], error.response.data.message);
+            disableLoading()
+          }
+        
+        }
+      );
+    }
   };
+ const signinUser = () => {
+  enableLoading()
+    toastNotification(["info"],('logging in... '))
+    props.signIn(userSignInDetails)
+      .then(() => {
+        toastNotification(['success'], `Welcome back <br/><em>${userSignInDetails.authname}</em>`);
+      /*   setTimeout(() => {
+          window.location = '/';
+        }, 300) */;disableLoading()
+      },
+      (error) => {
+        disableLoading()
+      if(!error.response){
+        toastNotification(["error"], 'Network Error');
+      }
+      else{
+        toastNotification(["error"], error.response.data.message);
+        disableLoading()
+      }
+      });
+  }
   return (
     <>
       <div class="header">
@@ -64,15 +130,25 @@ function Authpage() {
           <Intro />
           <SignupForm
             showSignup={showSignup}
-            handleInputChange={saveSignupToState}
+            saveSignupToState={saveSignupToState}
             signupUser={signupUser}
             fullname={userSignUpDetails.fullname}
             username={userSignUpDetails.username}
             email={userSignUpDetails.email}
             password={userSignUpDetails.password}
             repeatPassword={userSignUpDetails.repeatPassword}
+            submitValue={submitValue}
+            isLoading={isLoading}
           />
-          <SigninForm showSignin={showSignin} />
+          <SigninForm
+          showSignin={showSignin} 
+          saveSigninToState={saveSigninToState}
+          authname={userSignInDetails.authname}
+          password={userSignInDetails.password}
+          submitValue={submitValue}
+          isLoading={isLoading}
+          signinUser={signinUser}
+          />
         </div>
       </div>
       <Main />
@@ -80,4 +156,4 @@ function Authpage() {
     </>
   );
 }
-export default Authpage;
+export default connect(null, {signUp,signIn})(Authpage);
