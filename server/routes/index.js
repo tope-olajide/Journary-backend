5f1a72c64bd8a45281bb9f9429d4b0be3bc88f95
Temitope <tope_olajide@outlook.com>
2019-03-25 09:14:46 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import users from './user';
+import entries from './entry'
 
 const routes = express.Router();
 
 routes.use('/user', users);
+routes.use('/entry', entries);
 
 
 export default routes;
