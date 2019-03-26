e6174e5ba3a8c2c10f75a4e99a0ed30a5cc8b8e9
Temitope <tope_olajide@outlook.com>
2019-03-26 07:53:30 +0100

    Update createTables.js

--- server/models/createTables.js
+++ server/models/createTables.js
@@ -5,14 +5,13 @@ export default {
   createUsers() {
     const usersSchema = `
     CREATE TABLE IF NOT EXISTS users (
-      userId SERIAL PRIMARY KEY,
+      user_id SERIAL PRIMARY KEY,
       email TEXT NOT NULL UNIQUE,
       fullname TEXT NOT NULL,
-      about TEXT NOT NULL,
+      about TEXT,
       username VARCHAR(255) NOT NULL UNIQUE,
-      password VARCHAR(255) NOT NULL,
-      userImageUrl TEXT,
-      view INTEGER DEFAULT 0
+      password TEXT NOT NULL,
+      user_image_url TEXT
     );
     `;
     return database.query(usersSchema);
@@ -21,28 +20,16 @@ export default {
   createEntries() {
     const entriesSchema = `
     CREATE TABLE IF NOT EXISTS entries (
-      entryId SERIAL PRIMARY KEY,
-      userId INTEGER REFERENCES users(userId),
-      entryImageUrl TEXT,
+      entry_id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(user_id),
+      entry_image_url TEXT,
       title TEXT NOT NULL,
-      isPrivate BOOLEAN DEFAULT TRUE NOT NULL,
+      is_private BOOLEAN DEFAULT TRUE NOT NULL,
       content TEXT NOT NULL,
       created_at TIMESTAMP (0) without time zone default now(),
       updated_at TIMESTAMP
     );
     `;
     return database.query(entriesSchema);
-  },
-  createFavourites() {
-    const favouritesSchema = `
-    CREATE TABLE IF NOT EXISTS favourites (
-      favouriteId SERIAL PRIMARY KEY,
-      entryId INTEGER REFERENCES entries(entryId),
-      userId INTEGER REFERENCES users(userId),
-      created_at TIMESTAMP (0) without time zone default now(),
-      updated_at TIMESTAMP  
-    );
-    `;
-    return database.query(favouritesSchema);
   },
 };
