a9d22449471a10ae4632597435dc3eb43a06b05f
Temitope <tope_olajide@outlook.com>
2019-03-26 07:53:53 +0100

    Update index.js

--- server/routes/entry/index.js
+++ server/routes/entry/index.js
@@ -9,8 +9,23 @@ const newAuth = new Auth();
 const entry = express.Router();
 
 entry.use('*', newAuth.verify);
+entry.route('/search')
+  .get(Entry.searchEntries)
 entry.route('/')
   .post(Entry.addEntry)
+  .get(Entry.getAllPublicEntries);
+
+entry.route('/private')
+  .get(Entry.getUserPrivateEntries);
+
+entry.route('/public')
   .get(Entry.getUserPublicEntries);
+
+entry.route('/:entryId')
+  .put(Entry.modifyEntry)
+  .delete(Entry.deleteEntry)
+  .get(Entry.viewEntry);
+
+
 
 export default entry;
