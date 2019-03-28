9af4fc830354514cf3ee2c528f3c4d34aecb900f
Temitope <tope_olajide@outlook.com>
2019-03-28 08:26:48 +0100

    Update encryption.js

--- server/middleware/encryption.js
+++ server/middleware/encryption.js
@@ -1,43 +1,30 @@
+/* eslint-disable require-jsdoc */
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
-/**
- * @description - Class Definition for the Encryption Object
- *
- * @export
- *
- * @class Encryption
- */
 export default class Encryption {
-  /**
-   * @description - Generate Hash for password string
-   *
-   * @param {string} password - User password
-   *
-   * @return {string} hashedPassword - hashed password
-   *
-   * @memberof Encryption
-   */
   generateHash(password) {
     this.salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, this.salt);
-
     return hash;
   }
 
-
-  /**
-   * @description - Verify/Decrypt password
-   *
-   * @exports verifyHash
-   *
-   * @param  {string} password - User Password
-   *
-   * @param  {string} hash - hashed User Password
-   *
-   * @return {boolean} status - The status of decryption
-   */
   verifyHash(password, hash) {
     this.status = bcrypt.compareSync(password, hash);
     return this.status;
+  }
+
+  encryptDiary(entry) {
+    const key = crypto.createCipher('aes-128-cbc', 'mypassword');
+    let encryptedEntry = key.update(entry, 'utf8', 'hex');
+    encryptedEntry += key.update.final('hex');
+    return encryptedEntry;
+  }
+
+  decrytDiary(entry) {
+    const key = crypto.createDecipher('aes-128-cbc', 'mypassword');
+    let decryptedEntry = key.update(entry, 'hex', 'utf8');
+    decryptedEntry += key.update.final('utf8');
+    return decryptedEntry;
   }
 }
