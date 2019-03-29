2bd9ac88d0c118744e6ea2fc9a91b865b5b2a091
Temitope <tope_olajide@outlook.com>
2019-03-29 20:08:32 +0100

    Update Navigation.jsx

--- client/src/components/commons/Navigation.jsx
+++ client/src/components/commons/Navigation.jsx
@@ -1,41 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Search from "../commons/Search";
-
- const NavBar=()=> {
-    const [showNav, setShowNav] = useState(false);
-    const [displaySearch, setDisplaySearch] = useState(false);
-    const toggleSearch = () => {
-        setDisplaySearch(!displaySearch);
-      };
-    const toggleNav = ()=>{
-        setShowNav(!showNav)
-    }
-    return (
-        <>
-<div className="header-container">
-    <div className="top-nav-container">
-        <div className="logo">
+import { Route, Link } from "react-router-dom";
+const NavBar = () => {
+  const [showNav, setShowNav] = useState(false);
+  const [displaySearch, setDisplaySearch] = useState(false);
+  const toggleSearch = () => {
+    setDisplaySearch(!displaySearch);
+  };
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+  return (
+    <>
+      <div className="header-container">
+        <div className="top-nav-container">
+          <div className="logo">
             <h1>MY-DIARY</h1>
+          </div>
+          <div className={showNav ? "change" : "menu-icon"} onClick={toggleNav}>
+            <div className="bar1" />
+            <div className="bar2" />
+            <div className="bar3" />
+          </div>
+          <div className={showNav ? "show-topnav" : "topnav"}>
+            <Link to="/" className="active">
+              <i className="fas fa-home" /> Home
+            </Link>
+            <a
+              href="#search"
+              onClick={event => {
+                event.preventDefault();
+                toggleSearch();
+              }}
+            >
+              <i className="fas fa-search" /> Search
+            </a>
+            <Link to="/add-entry">
+              <i className="fas fa-edit" /> New
+            </Link>
+            <Link to="/profile">
+              <i className="fas fa-user" /> Profile
+            </Link>
+            <Link to="/logout">
+              <i className="fas fa-sign-out-alt" /> Logout
+            </Link>
+          </div>
         </div>
-        <div className={showNav?"change":"menu-icon"} onClick={toggleNav}>
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
-        </div>
-        <div className={showNav?"show-topnav":"topnav"}>
-            <a href="homepage.html" className="active"><i className="fas fa-home"></i> Home</a>
-            <a href="#search" onClick={event => {
-              event.preventDefault();
-              toggleSearch();
-            }}><i className="fas fa-search"></i> Search</a>
-            <a href="add-entry.html"><i className="fas fa-edit"></i> New</a>
-            <a href="profile.html"><i className="fas fa-user"></i> Profile</a>
-            <a href="index.html"><i className="fas fa-sign-out-alt"></i> Logout</a>
-        </div>
-    </div>
-</div>
-<Search displaySearch={displaySearch} closeSearch={toggleSearch} />
-        </>
-    )
-}
-export default NavBar
+      </div>
+      <Search displaySearch={displaySearch} closeSearch={toggleSearch} />
+    </>
+  );
+};
+export default NavBar;
