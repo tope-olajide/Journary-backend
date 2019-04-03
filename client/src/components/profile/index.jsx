96e264061a1b4286f20824f85536723d44193287
Temitope <tope_olajide@outlook.com>
2019-04-03 09:24:01 +0100

    Update Profile Component

--- client/src/components/profile/index.jsx
+++ client/src/components/profile/index.jsx
@@ -1,46 +1,61 @@
-import React, { useState } from 'react';
-import NavigationBar from '../commons/Navigation'
-import Footer from '../commons/Footer'
-
-const Profile =()=>{
-    const [isDisplaySideNav, setIsDisplaySideNav] = useState(false)
-    const toggleSideBar = ()=> {
-        setIsDisplaySideNav(!isDisplaySideNav)
-    }
-    return(
-     <>
-     <NavigationBar />
-       <div class="profile-header"><span class="sidenav-icon" onClick={toggleSideBar}>&#9776; My Entries</span>
-      <div id="profileSidenav" class={isDisplaySideNav?"show-sidenav":"hide-sidenav"}>
-        <a href="javascript:void(0)" class="closebtn" onClick={toggleSideBar}>&times;</a>
-        <a href="private.html"><i class="fas fa-lock"></i> Private</a>
-        <a href="public.html"><i class="fas fa-lock-open"></i> Public</a>
-        <a href="settings.html"><i class="fas fa-gear"></i> Settings</a>
-      </div>
-      <br /><br /><div class="profile-picture">
-     <div class="profile-overlay"> </div><img class="profile-picture-img" src="./images/avatar.jpg" alt="" />
-  </div>
-  <div class="profile-info">
-    <h2 class="about">Mary Jane</h2>
-    <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut Lorem ipsum dolor sit amet</p>
-    <h3>Entries</h3>
-    <table>
-        <tr>
-          <th>Private</th>
-          <th>Public</th>
-          <th>Total</th>
-        </tr>
-        <tr>
-          <td>10</td>
-          <td>223</td>
-          <td>23</td>
-        </tr>
-      </table>
-      <button class="edit-profile__button">Edit Profile</button>
-  </div>
-</div>
-<Footer />
-</>
-    )
-}
-export default Profile
+import React, { useState, useEffect } from "react";
+import UserProfile from "./UserProfile";
+import NavigationBar from "../commons/Navigation";
+import Footer from "../commons/Footer";
+import { fetchProfileDetails } from "../../actions/userActions";
+import { connect } from "react-redux";
+const Profile = ({ dispatch,userDetails,privateEntriesCount,publicEntriesCount,totalEntriesCount }) => {
+  const [isDisplaySideNav, setIsDisplaySideNav] = useState(false);
+  const toggleSideBar = () => {
+    setIsDisplaySideNav(!isDisplaySideNav);
+  };
+  const [isLoading, setIsloading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+  const fetchUserProfile = () => {
+    dispatch(fetchProfileDetails())
+      .then(() => {
+        console.log("success");
+        setIsloading(false);
+      })
+      .catch(error => {
+        console.log("fail");
+        setIsloading(false);
+        setIsError(true)
+      });
+  };
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  } else if (isError) {
+    return <h1>Error!</h1>;
+  } else {
+    return (
+      <>
+        <NavigationBar />
+        <UserProfile
+          toggleSideBar={toggleSideBar}
+          isDisplaySideNav={isDisplaySideNav}
+          fullname={userDetails.fullname}
+          userImageUrl={userDetails.user_image_url}
+          about={userDetails.about}
+          privateCount={privateEntriesCount}
+          publicCount={publicEntriesCount}
+          totalCount={totalEntriesCount}
+        />
+        <Footer />
+      </>
+    );
+  }
+};
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    userDetails:state.users.userData[0],
+    privateEntriesCount:state.users.privateEntriesCount,
+    publicEntriesCount:state.users.publicEntriesCount,
+    totalEntriesCount: state.users.totalEntriesCount
+  };
+};
+export default connect(mapStateToProps)(Profile);
