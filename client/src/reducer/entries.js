11e629d3bb34e8a1f1ed8e0072a0d3df00e92801
Temitope <tope_olajide@outlook.com>
2019-04-03 09:25:58 +0100

    Update entries Reducer

--- client/src/reducer/entries.js
+++ client/src/reducer/entries.js
@@ -1,19 +1,28 @@
-import {FETCH_ALL_PUBLIC_ENTRIES
-  } from '../actions/type'
-  
-  const initialState = {
-    entries: {},
+import {
+  FETCH_ALL_PUBLIC_ENTRIES,FETCH_ENTRY_DETAILS
+} from '../actions/type'
+
+const initialState = {
+  entries: {
     currentPage: {}
+  },
+  entryDetails:{}
+
+}
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_ALL_PUBLIC_ENTRIES:
+      return {
+        ...state,
+        entries: action.entries,
+        currentPage: action.currentPage
+      };
+    case FETCH_ENTRY_DETAILS:
+      return {
+        ...state,
+        entryDetails: action.entry
+      };
+    default:
+      return state;
   }
-  export default (state = initialState, action) => {
-    switch (action.type) {
-      case FETCH_ALL_PUBLIC_ENTRIES:
-        return {
-          ...state,
-          entries: action.entries,
-          currentPage: action.currentPage
-        };
-      default:
-        return state;
-    }
-  }
+}
