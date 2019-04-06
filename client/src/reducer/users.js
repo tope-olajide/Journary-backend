96d98b2c41ffc795c3390a4277fa3d4e87296f5a
Temitope <tope_olajide@outlook.com>
2019-04-06 04:30:49 +0100

    Update users.js

--- client/src/reducer/users.js
+++ client/src/reducer/users.js
@@ -1,25 +1,38 @@
-
 import {
-    FETCH_USER_DETAILS
-  } from '../actions/type'
-  
-  const initialState = {
-        userData: {},
-        privateEntriesCount:{},
-        publicEntriesCount:{},
-        totalEntriesCount:{}
+  FETCH_USER_DETAILS,
+  UPDATED_USER_DETAILS,
+  GET_REMINDER_SETTINGS
+} from '../actions/type'
+
+const initialState = {
+  userData: {},
+  privateEntriesCount: {},
+  publicEntriesCount: {},
+  totalEntriesCount: {},
+  updatedUser: {},
+  reminderSettings:{}
+}
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_DETAILS:
+      return {
+        ...state,
+        userData: action.userData,
+        privateEntriesCount: action.privateEntriesCount,
+        publicEntriesCount: action.publicEntriesCount,
+        totalEntriesCount: action.totalEntriesCount
+      };
+    case UPDATED_USER_DETAILS:
+      return {
+        ...state,
+        updatedUser: action.updatedUser
+      }
+    case GET_REMINDER_SETTINGS:
+      return {
+        ...state,
+        reminderSettings: action.reminder
+      };
+    default:
+      return state;
   }
-  export default (state = initialState, action) => {
-    switch (action.type) {
-      case FETCH_USER_DETAILS:
-        return {
-          ...state,
-          userData: action.userData,
-          privateEntriesCount: action.privateEntriesCount,
-          publicEntriesCount: action.publicEntriesCount,
-          totalEntriesCount: action.totalEntriesCount
-        };
-      default:
-        return state;
-    }
-  }
+}
