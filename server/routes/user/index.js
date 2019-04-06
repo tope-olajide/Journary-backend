fdfbf4f3e75f866af894a1754959802937e06fba
Temitope <tope_olajide@outlook.com>
2019-04-06 04:31:48 +0100

    Update index.js

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -10,5 +10,8 @@ const user = express.Router();
 user.post('/signup', User.signupUser);
 user.post('/signin', User.signInUser);
 user.use('*', newAuth.verify);
+user.put('/update-profile', User.modifyUser);
 user.get('/', User.getUser);
+user.get('/get-reminder', User.getReminderSettings);
+user.post('/set-reminder', User.setReminderSettings);
 export default user;
