ac99bfbff7c64a1d651e16d5150f2b54cc4ce889
Temitope <tope_olajide@outlook.com>
2020-02-15 16:21:43 +0100

    Update createTable.js

--- models/createTables.js
+++ models/createTables.js
@@ -5,13 +5,19 @@ export default {
   createUsers() {
     const usersSchema = `
     CREATE TABLE IF NOT EXISTS users (
-      user_id SERIAL PRIMARY KEY
-
+      user_id SERIAL PRIMARY KEY,
+      email TEXT NOT NULL UNIQUE,
+      fullname TEXT NOT NULL,
+      about TEXT,
+      username VARCHAR(255) NOT NULL UNIQUE,
+      password TEXT NOT NULL,
+      user_image_url TEXT,
+      notification_settings TEXT DEFAULT "Off" 
     );
     `;
     return database.query(usersSchema);
   }
-  /*  createImageGalleries() {
+ /*  createImageGalleries() {
     const galleriesSchema = `
     CREATE TABLE IF NOT EXISTS image_galleries (
       gallery_id SERIAL PRIMARY KEY,
@@ -30,7 +36,7 @@ export default {
       entry_image_url TEXT,
       title TEXT NOT NULL,
       is_private BOOLEAN DEFAULT TRUE NOT NULL,
-      view_count INTEGER DEFAULT 0,
+      view_count INTEGER DEFAULT 0, 
       content TEXT NOT NULL,
       created_at TIMESTAMP (0) without time zone default now(),
       updated_at TIMESTAMP
