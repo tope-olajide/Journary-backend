8b26eacebf84bae2b1ad45bfc76ea66343d82b67
Temitope <tope_olajide@outlook.com>
2020-03-24 19:04:53 +0100

    Update reminder controller

--- controllers/reminder.js
+++ controllers/reminder.js
@@ -1,19 +1,30 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable require-jsdoc */
 import db from '../db';
 import scheduleTask, { cancelTask } from '../utils/scheduler';
 
-
+/**
+ * @description - Class Definition for the Reminder Object
+ *
+ * @export
+ *
+ * @class Reminder
+ */
 export default class Reminder {
-  static async getReminderSettings({
-    user
-  }, res) {
+  /**
+   * @description - Fetch reminder settings
+   *
+   * @param {object} req - HTTP Request
+   *
+   * @param {object} res - HTTP Response
+   *
+   * @return {json} Returns json object
+   *
+   * @memberof Reminder
+   */
+  static async getReminderSettings({ user }, res) {
     const userId = user.id;
     const text = 'SELECT reminder_time FROM reminders WHERE user_id = $1';
     try {
-      const {
-        rows
-      } = await db.query(text, [userId]);
+      const { rows } = await db.query(text, [userId]);
       if (!rows[0]) {
         return res.status(200).json({
           success: true,
@@ -24,7 +35,7 @@ export default class Reminder {
       return res.status(200).json({
         success: true,
         message: 'Reminder found',
-        reminder: rows[0].reminder_time,
+        reminder: rows[0].reminder_time
       });
     } catch (error) {
       console.log(error);
@@ -32,18 +43,24 @@ export default class Reminder {
     }
   }
 
-  static async setReminderSettings({
-    user,
-    body
-  }, res) {
+  /**
+   * @description - Creates new reminder
+   *
+   * @param {object} req - HTTP Request
+   *
+   * @param {object} res - HTTP Response
+   *
+   * @return {json} Returns json object
+   *
+   * @memberof Reminder
+   */
+  static async setReminderSettings({ user, body }, res) {
     const userId = user.id;
     const { email } = user;
     const { username } = user;
-    const {
-      time
-    } = body;
+    const { time } = body;
     console.log(time);
-    console.log(username)
+    console.log(username);
     const taskId = `user00${userId}`;
     const deleteQuery = 'DELETE FROM reminders WHERE user_id =$1 returning *';
     /* const saveUserEmail = 'UPDATE users SET reminder_time=$1 WHERE user_id=$2 returning *'; */
@@ -51,13 +68,7 @@ export default class Reminder {
       VALUES($1, $2, $3, $4,$5)
       returning *`;
     const updateReminderSettings = 'UPDATE reminders SET reminder_time =$1 WHERE task_id=$2 returning *';
-    const values = [
-      time,
-      taskId,
-      userId,
-      email,
-      username
-    ];
+    const values = [time, taskId, userId, email, username];
     const value3 = [time, taskId];
     const searchForUser = 'SELECT * FROM reminders WHERE username = $1';
     try {
@@ -66,7 +77,7 @@ export default class Reminder {
         cancelTask(taskId);
         return res.status(200).json({
           success: true,
-          message: 'Email Reminder Deactivated Successfully',
+          message: 'Email Reminder Deactivated Successfully'
         });
       }
       const userExist = await db.query(searchForUser, [username]);
@@ -93,6 +104,17 @@ export default class Reminder {
     }
   }
 
+  /**
+   * @description - Fetches all the Reminder in the database
+   *
+   * @param {object} req - HTTP Request
+   *
+   * @param {object} res - HTTP Response
+   *
+   * @return {json} Returns json object
+   *
+   * @memberof Reminder
+   */
   static async getAllReminders(req, res) {
     const findAllReminders = 'SELECT * FROM reminders';
     try {
@@ -115,6 +137,18 @@ export default class Reminder {
     }
   }
 
+  /**
+   * @description - Restart all the reminders
+   * incase the app crashed or stopped working
+   *
+   * @param {object} req - HTTP Request
+   *
+   * @param {object} res - HTTP Response
+   *
+   * @return {json} Returns json object
+   *
+   * @memberof Reminder
+   */
   static async setAllReminders(req, res) {
     const getAllReminders = 'SELECT * FROM reminders';
     try {
@@ -127,7 +161,12 @@ export default class Reminder {
         });
       }
       allReminders.rows.map((eachReminder) => {
-        scheduleTask(eachReminder.task_id, eachReminder.reminder_time,eachReminder.username,eachReminder.email);
+        scheduleTask(
+          eachReminder.task_id,
+          eachReminder.reminder_time,
+          eachReminder.username,
+          eachReminder.email
+        );
       });
       return res.status(200).json({
         success: true,
@@ -140,17 +179,26 @@ export default class Reminder {
     }
   }
 
+  /**
+   * @description - Stop and delete a user reminder
+   *
+   * @param {object} req - HTTP Request
+   *
+   * @param {object} res - HTTP Response
+   *
+   * @return {json} Returns json object
+   *
+   * @memberof Reminder
+   */
   static async unSubscribe({ params }, res) {
-    const {
-      email
-    } = params;
-    console.log(email)
+    const { email } = params;
+    console.log(email);
     const searchForEmail = 'SELECT * FROM reminders WHERE email = $1';
     const deleteReminder = 'DELETE FROM reminders WHERE email =$1 returning *';
     try {
       const user = await db.query(searchForEmail, [email]);
       if (!user.rows) {
-        console.log(user)
+        console.log(user);
         return res.status(200).json({
           success: true,
           message: 'Reminder Deactivated Already!',
@@ -163,7 +211,7 @@ export default class Reminder {
       cancelTask(taskId);
       return res.status(200).json({
         success: true,
-        message: 'Your Email Reminder has been Deactivated Successfully',
+        message: 'Your Email Reminder has been Deactivated Successfully'
       });
     } catch (error) {
       console.log(error);
