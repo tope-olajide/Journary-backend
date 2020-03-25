25fe4b3ef5b5387e240d725421a6fa70521f926e
Temitope <tope_olajide@outlook.com>
2020-03-25 19:06:38 +0100

    Update encryption middleware

--- middleware/encryption.js
+++ middleware/encryption.js
@@ -1,19 +1,44 @@
-/* eslint-disable require-jsdoc */
 import bcryptjs from 'bcryptjs';
-import crypto from 'crypto';
-
+/* import crypto from "crypto"; */
+/**
+ * @description - Class Definition for the Encryption Object
+ *
+ * @export
+ *
+ * @class Encryption
+ */
 export default class Encryption {
+  /**
+   * @description - Encrypts user password before saving into database
+   *
+   * @param {string} password - user password
+   *
+   * @return {string} Returns harshed password
+   *
+   * @memberof Encryption
+   */
   generateHash(password) {
     this.salt = bcryptjs.genSaltSync(10);
     const hash = bcryptjs.hashSync(password, this.salt);
     return hash;
   }
 
+  /**
+   * @description - Verify Harshed Password
+   *
+   * @param {string} password - user password
+   *
+   * @param {string} harsh - harshed password
+   *
+   * @return {boolean} Returns true or false
+   *
+   * @memberof Encryption
+   */
   verifyHash(password, hash) {
     this.status = bcryptjs.compareSync(password, hash);
     return this.status;
   }
-
+  /*
   encryptDiary(entry) {
     const key = crypto.createCipher('aes-128-cbc', 'mypassword');
     let encryptedEntry = key.update(entry, 'utf8', 'hex');
@@ -26,5 +51,5 @@ export default class Encryption {
     let decryptedEntry = key.update(entry, 'hex', 'utf8');
     decryptedEntry += key.update.final('utf8');
     return decryptedEntry;
-  }
+  } */
 }
